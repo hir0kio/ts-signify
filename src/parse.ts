6290046a6b52d14317de29da50e7d84b98aa96e9
Hiroki Okada <hirokio@tutanota.com>
2022-01-04 04:53:27 +0900

    refactor(parse): avoid redundancy

--- src/parse.ts
+++ src/parse.ts
@@ -10,14 +10,16 @@ import {
 
 function _parseContainer<T extends PrivateKey | PublicKey | Signature>(
   input: Buffer | string,
-  parseContent: (comment: string, content: string) => T | null
+  parseContent: (comment: string, content: Buffer) => T | null
 ): T | null {
   try {
     let split = input.toString().split("\n", 2);
     let comment = split[0].match(/^untrusted comment: (.*?)$/)![1];
     let content = split[1];
 
-    return content ? parseContent(comment, content) : null;
+    return content
+      ? parseContent(comment, Buffer.from(content, "base64"))
+      : null;
   } catch (e) {
     return null;
   }
@@ -25,17 +27,16 @@ function _parseContainer<T extends PrivateKey | PublicKey | Signature>(
 
 export function parsePrivateKey(input: Buffer | string): PrivateKey | null {
   return _parseContainer<PrivateKey>(input, (comment, content) => {
-    let decoded = Buffer.from(content, "base64"),
-      privateKey = {
-        comment,
-        algorithm: decoded.subarray(0, 2).toString(),
-        kdfAlgorithm: decoded.subarray(2, 4).toString(),
-        kdfRounds: _buf2num(decoded.subarray(4, 8)),
-        salt: decoded.subarray(8, 24),
-        checksum: decoded.subarray(24, 32),
-        keyNumber: decoded.subarray(32, 40),
-        content: decoded.subarray(40, 104),
-      };
+    let privateKey = {
+      comment,
+      algorithm: content.subarray(0, 2).toString(),
+      kdfAlgorithm: content.subarray(2, 4).toString(),
+      kdfRounds: _buf2num(content.subarray(4, 8)),
+      salt: content.subarray(8, 24),
+      checksum: content.subarray(24, 32),
+      keyNumber: content.subarray(32, 40),
+      content: content.subarray(40, 104),
+    };
 
     return checkPrivateKey(privateKey) ? privateKey : null;
   });
@@ -43,13 +44,12 @@ export function parsePrivateKey(input: Buffer | string): PrivateKey | null {
 
 export function parsePublicKey(input: Buffer | string): PublicKey | null {
   return _parseContainer(input, (comment, content) => {
-    let decoded = Buffer.from(content, "base64"),
-      publicKey = {
-        comment,
-        algorithm: decoded.subarray(0, 2).toString(),
-        keyNumber: decoded.subarray(2, 10),
-        content: decoded.subarray(10, 42),
-      };
+    let publicKey = {
+      comment,
+      algorithm: content.subarray(0, 2).toString(),
+      keyNumber: content.subarray(2, 10),
+      content: content.subarray(10, 42),
+    };
 
     return checkPublicKey(publicKey) ? publicKey : null;
   });
@@ -57,13 +57,12 @@ export function parsePublicKey(input: Buffer | string): PublicKey | null {
 
 export function parseSignature(input: Buffer | string): Signature | null {
   return _parseContainer(input, (comment, content) => {
-    let decoded = Buffer.from(content, "base64"),
-      signature = {
-        comment,
-        algorithm: decoded.subarray(0, 2).toString(),
-        keyNumber: decoded.subarray(2, 10),
-        content: decoded.subarray(10, 74),
-      };
+    let signature = {
+      comment,
+      algorithm: content.subarray(0, 2).toString(),
+      keyNumber: content.subarray(2, 10),
+      content: content.subarray(10, 74),
+    };
 
     return checkSignature(signature) ? signature : null;
   });
